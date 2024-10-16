1d228b696d34d61b9bb38aa12e928be6b89c7f02
jeanport <pierreanatc522@gmail.com>
2024-10-16 14:22:19 -0300

    Configurando banco de dados

---
+++ src/main/java/br/com/truedev/ecomerci/EcomerciApplication.java
@@ -0,0 +1,13 @@
+package br.com.truedev.ecomerci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/truedev/ecomerci/EcomerciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.truedev.ecomerci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
